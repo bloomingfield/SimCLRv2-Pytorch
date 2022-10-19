1676d27d68f40cc06f052518e4a6c84aaf8cec9d
Nathaniel Bloomfield <nathaniel.bloomfield@unimelb.edu.au>
2022-10-19 16:15:51 +1100

    have code to translate cifar10 tf resnet models to pytorch

--- convert.py
+++ convert.py
@@ -4,119 +4,146 @@ import torch
 import numpy as np
 import torch.nn as nn
 import tensorflow as tf
+from tensorflow.python.training import checkpoint_utils as cp
 
-from resnet import get_resnet, name_to_params
+# python convert.py  ../simclr/tf2/cifar10_models/firsttry_real/ckpt-780
+from resnet import get_resnet, get_head
+from pdb import set_trace as pb
+import re
 
 parser = argparse.ArgumentParser(description='SimCLR converter')
 parser.add_argument('tf_path', type=str, help='path of the input tensorflow file (ex: model.ckpt-250228)')
-parser.add_argument('--ema', action='store_true')
-parser.add_argument('--supervised', action='store_true')
 args = parser.parse_args()
 
-
 def main():
-    use_ema_model = args.ema
-    prefix = ('ema_model/' if use_ema_model else '') + 'base_model/'
-    head_prefix = ('ema_model/' if use_ema_model else '') + 'head_contrastive/'
+    prefix = 'resnet_model/'
+    head_prefix = 'projection_head/'
+
+    tf_variables = cp.list_variables(args.tf_path)
+    # tf_values = cp.load_variables(args.tf_path)
+
     # 1. read tensorflow weight into a python dict
     vars_list = []
     contrastive_vars = []
-    for v in tf.train.list_variables(args.tf_path):
-        if v[0].startswith(prefix) and not v[0].endswith('/Momentum'):
+    for v in tf_variables:
+        if prefix in v[0] and not v[0].endswith('/Momentum/.ATTRIBUTES/VARIABLE_VALUE'):
             vars_list.append(v[0])
         elif v[0] in {'head_supervised/linear_layer/dense/bias', 'head_supervised/linear_layer/dense/kernel'}:
             vars_list.append(v[0])
-        elif v[0].startswith(head_prefix) and not v[0].endswith('/Momentum'):
+        elif head_prefix in v[0] and not v[0].endswith('/Momentum/.ATTRIBUTES/VARIABLE_VALUE'):
             contrastive_vars.append(v[0])
 
     sd = {}
-    ckpt_reader = tf.train.load_checkpoint(args.tf_path)
+    # ckpt_reader = tf.train.load_checkpoint(args.tf_path)
     for v in vars_list:
-        sd[v] = ckpt_reader.get_tensor(v)
+        variable = cp.load_variable(args.tf_path, v)
+        v = re.sub('/.ATTRIBUTES/VARIABLE_VALUE', '', v)
+        sd[v] = variable
 
-    split_idx = 2 if use_ema_model else 1
     # 2. convert the state_dict to PyTorch format
-    conv_keys = [k for k in sd.keys() if k.split('/')[split_idx].split('_')[0] == 'conv2d']
-    conv_idx = []
-    for k in conv_keys:
-        mid = k.split('/')[split_idx]
-        if len(mid) == 6:
-            conv_idx.append(0)
-        else:
-            conv_idx.append(int(mid[7:]))
-    arg_idx = np.argsort(conv_idx)
-    conv_keys = [conv_keys[idx] for idx in arg_idx]
+    conv_keys = [k for k in sd.keys() if '/conv2d/' in k]
+    conv_keys = [re.sub('/kernel', '', x) for x in conv_keys]
+    conv_keys.sort()
+    bn_keys = [k for k in sd.keys() if '/bn/' in k]
+    bn_keys = [re.sub('/gamma|/beta|/moving_mean|/moving_variance', '', x) for x in bn_keys]
+    bn_keys = list(set(bn_keys))
+    bn_keys.sort()
 
-    bn_keys = list(set([k.split('/')[split_idx] for k in sd.keys()
-                        if k.split('/')[split_idx].split('_')[0] == 'batch']))
-    bn_idx = []
-    for k in bn_keys:
-        if len(k.split('_')) == 2:
-            bn_idx.append(0)
-        else:
-            bn_idx.append(int(k.split('_')[2]))
-    arg_idx = np.argsort(bn_idx)
-    bn_keys = [bn_keys[idx] for idx in arg_idx]
+    # depth, width, sk_ratio = name_to_params(args.tf_path)
+    depth = 18
+    width = 1
+    sk_ratio = 0
 
-    depth, width, sk_ratio = name_to_params(args.tf_path)
-    model, head = get_resnet(depth, width, sk_ratio)
+    channels_in=512
+    num_layers=2
+    out_dim=64
+
+    model = get_resnet(depth, width, sk_ratio, cifar_stem=True)
+    head = get_head(channels_in, num_layers, out_dim)
 
     conv_op = []
+    conv_op_names = []
     bn_op = []
-    for m in model.modules():
+    bn_op_names = []
+    for n, m in model.named_modules():
         if isinstance(m, nn.Conv2d):
             conv_op.append(m)
+            conv_op_names.append(n)
         elif isinstance(m, nn.BatchNorm2d):
             bn_op.append(m)
-    assert len(vars_list) == (len(conv_op) + len(bn_op) * 4 + 2)  # 2 for fc
+            bn_op_names.append(n)
+    assert len(vars_list) == (len(conv_op) + len(bn_op) * 4 )  # 2 for fc
 
-    for i_conv in range(len(conv_keys)):
-        m = conv_op[i_conv]
-        w = torch.from_numpy(sd[conv_keys[i_conv]]).permute(3, 2, 0, 1)
-        assert w.shape == m.weight.shape, f'size mismatch {w.shape} <> {m.weight.shape}'
+    # first block
+    # initial_conv_relu_max_pool = net.0
+    # block_groups/j/conv2d_bn_layers/i = net.j.blocks.i.net
+    # block_groups/j/shortcut_layers/i = net.j.blocks.0.projection.shortcut
+
+    def use_key_conv(m, key):
+        w = torch.from_numpy(sd[key+'/kernel']).permute(3, 2, 0, 1)
         m.weight.data = w
 
-    for i_bn in range(len(bn_keys)):
-        m = bn_op[i_bn]
-        gamma = torch.from_numpy(sd[prefix + bn_keys[i_bn] + '/gamma'])
-        assert m.weight.shape == gamma.shape, f'size mismatch {gamma.shape} <> {m.weight.shape}'
+    def use_key_linear(m, key):
+        w = torch.from_numpy(sd[key+'/kernel'])
+        m.weight.data = w
+
+    def use_key_bn(m, key):
+        gamma = torch.from_numpy(sd[key + '/gamma'])
         m.weight.data = gamma
-        m.bias.data = torch.from_numpy(sd[prefix + bn_keys[i_bn] + '/beta'])
-        m.running_mean = torch.from_numpy(sd[prefix + bn_keys[i_bn] + '/moving_mean'])
-        m.running_var = torch.from_numpy(sd[prefix + bn_keys[i_bn] + '/moving_variance'])
+        if (m.bias is not None):
+            m.bias.data = torch.from_numpy(sd[key + '/beta'])
+        m.running_mean = torch.from_numpy(sd[key + '/moving_mean'])
+        m.running_var = torch.from_numpy(sd[key + '/moving_variance'])
+        
 
-    w = torch.from_numpy(sd['head_supervised/linear_layer/dense/kernel']).t()
-    assert model.fc.weight.shape == w.shape
-    model.fc.weight.data = w
-    b = torch.from_numpy(sd['head_supervised/linear_layer/dense/bias'])
-    assert model.fc.bias.shape == b.shape
-    model.fc.bias.data = b
+    # setting up stem.... done!
+    key = 'model/resnet_model/initial_conv_relu_max_pool/0/conv2d'
+    use_key_conv(model.net[0].conv1, key)
 
-    if args.supervised:
-        save_location = f'r{depth}_{width}x_sk{1 if sk_ratio != 0 else 0}{"_ema" if use_ema_model else ""}.pth'
-        torch.save({'resnet': model.state_dict(), 'head': head.state_dict()}, save_location)
-        return
+    key = 'model/resnet_model/initial_conv_relu_max_pool/2/bn'
+    use_key_bn(model.net[0].bn1.bn, key)
+
+    for i in range(4):
+        layer_keys = [x for x in conv_keys if 'block_groups/'+str(i)+'/layers' in x]
+        layer_shortcut_key = [x for x in layer_keys if 'shortcut_layers' in x]
+        layer_conv_key = [x for x in layer_keys if 'conv2d_bn_layers' in x]
+        layer_keys_bn = [x for x in bn_keys if 'block_groups/'+str(i)+'/layers' in x]
+        layer_shortcut_key_bn = [x for x in layer_keys_bn if 'shortcut_layers' in x]
+        layer_conv_key_bn = [x for x in layer_keys_bn if 'conv2d_bn_layers' in x]
+
+        use_key_conv(model.net[i+1].blocks[0].projection.shortcut.conv, layer_shortcut_key[0])
+        use_key_bn(model.net[i+1].blocks[0].projection.bn.bn, layer_shortcut_key_bn[0])
+        j_i = 0
+        for j in range(len(model.net[i+1].blocks)):
+            use_key_conv(model.net[i+1].blocks[j].net.conv1, layer_conv_key[j_i])
+            use_key_conv(model.net[i+1].blocks[j].net.conv2, layer_conv_key[j_i+1])
+            use_key_bn(model.net[i+1].blocks[j].net.bn1.bn, layer_conv_key_bn[j_i])
+            use_key_bn(model.net[i+1].blocks[j].net.bn2.bn, layer_conv_key_bn[j_i+1])
+            j_i+=2
+          
+    
+    # ===========================================================
     sd = {}
+    # ckpt_reader = tf.train.load_checkpoint(args.tf_path)
     for v in contrastive_vars:
-        sd[v] = ckpt_reader.get_tensor(v)
-    linear_op = []
-    bn_op = []
-    for m in head.modules():
-        if isinstance(m, nn.Linear):
-            linear_op.append(m)
-        elif isinstance(m, nn.BatchNorm1d):
-            bn_op.append(m)
-    for i, (l, m) in enumerate(zip(linear_op, bn_op)):
-        l.weight.data = torch.from_numpy(sd[f'{head_prefix}nl_{i}/dense/kernel']).t()
-        common_prefix = f'{head_prefix}nl_{i}/batch_normalization/'
-        m.weight.data = torch.from_numpy(sd[f'{common_prefix}gamma'])
-        if i != 2:
-            m.bias.data = torch.from_numpy(sd[f'{common_prefix}beta'])
-        m.running_mean = torch.from_numpy(sd[f'{common_prefix}moving_mean'])
-        m.running_var = torch.from_numpy(sd[f'{common_prefix}moving_variance'])
+        variable = cp.load_variable(args.tf_path, v)
+        v = re.sub('/.ATTRIBUTES/VARIABLE_VALUE', '', v)
+        sd[v] = variable
 
-    # 3. dump the PyTorch weights.
-    save_location = f'r{depth}_{width}x_sk{1 if sk_ratio != 0 else 0}{"_ema" if use_ema_model else ""}.pth'
+    # 2. convert the state_dict to PyTorch format
+    linear_keys = [k for k in sd.keys() if '/dense/' in k]
+    linear_keys = [re.sub('/kernel', '', x) for x in linear_keys]
+    linear_keys.sort()
+    bn_keys = [k for k in sd.keys() if '/bn/' in k]
+    bn_keys = [re.sub('/gamma|/beta|/moving_mean|/moving_variance', '', x) for x in bn_keys]
+    bn_keys = list(set(bn_keys))
+    bn_keys.sort()  
+
+    for i in range(num_layers):
+        use_key_linear(getattr(head.layers, 'linear'+str(i)), linear_keys[i])
+        use_key_bn(getattr(head.layers, 'bn'+str(i)), bn_keys[i])
+
+    save_location = f'r{depth}_{width}x_simclrv2.pth'
     torch.save({'resnet': model.state_dict(), 'head': head.state_dict()}, save_location)
 
 
